224075f64307c1ad3e2b31ba7df8cedd083f4e7c
brunomry <brunomadozzo@gmail.com>
2024-02-05 04:15:00 -0300

    Corregi numero de ejercicio en componente TituloPrincipal

--- src/components/TituloPrincipal.jsx
+++ src/components/TituloPrincipal.jsx
@@ -4,7 +4,7 @@ const TituloPrincipal = () => {
   return (
     <section className="text-center text-white py-4 mb-5 bg-primary w-100 rounded-2">
       <h1 className="display-5">
-        Ejercicio N°4 - Trabajo Práctico de React
+        Ejercicio N°5 - Trabajo Práctico de React
       </h1>
     </section>
   );
